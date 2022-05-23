ce1633d7012b2c0710d44a47cd70f7376fe0ea9c
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-05-23 12:13:28 -0400

    push to DOM

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,56 @@
-import React from 'react'
-
-const URL = 'http://localhost:9000/api/todos'
+import React from 'react';
+import axios from "axios";
+const URL = 'http://localhost:9000/api/todos';
 
 export default class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      todos: [],
+      error: ''
+    }
+  }
+
+  fetchAllTodos = (todosState) => {
+    console.log("fetching", todosState);
+    axios.get(URL)
+    .then(res => {
+      console.log(res.data);
+      this.setState({
+        ...this.state,
+        todos: res.data.data
+      })
+    })
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        error: err.response.data.message
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.fetchAllTodos(this.state.todos);
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">{this.state.error}</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>;
+            })
+          }
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
